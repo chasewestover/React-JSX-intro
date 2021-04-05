1d677592393b63f0751b1a7dffa7a910c421a88c
Chase Westover <chasejwestover@gmail.com>
2021-04-05 13:34:45 -0500

    parts 1-3

---
+++ part-3/App.jsx
@@ -0,0 +1,7 @@
+function App(props){
+  return <div>
+    <Person name="c" age={18} hobbies={["biking", "running", "diving"]}/>
+    <Person name="charmander" age={2} hobbies={["burning", "scorching"]}/>
+    <Person name="rumpelstiltskin" age={100} hobbies={["spinning gold thread"]}/>
+  </div>;
+}
